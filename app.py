12049c8410aaeea5eca51391cfbb3ddeff62a3fb
dev-alexmachado <alexmachado.ribeiro@gmail.com>
2026-09-14 15:23:16 -0300

    Commit do dia 14/09/2026

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import webview
 import pyautogui as auto
 from flask import Flask, render_template, request
 
 from datetime import date
+import os
+import sys
 
 
-app = Flask(__name__)
+if getattr(sys, 'frozen', False):
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
+    app = Flask(__name__, template_folder=template_folder)
+else:
+    app = Flask(__name__)
 
 @app.route("/")
 def index():
@@ -36,4 +43,12 @@ def commitar():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    window = webview.create_window(
+        title="NEM GIT AUTO",
+        url=app,
+        width=1000,
+        height=700,
+    )
+    
+    webview.start()
